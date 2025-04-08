ff5cfac1e178926379b6a8a354ff07cc48f4996e
Reem Ahmad <reemahmadn22@gmail.com>
2025-04-08 17:26:13 +0300

    add fillable and casts

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    protected $fillable = [
+        'title',
+        'content',
+        'user_id'
+    ];
+
+    protected $casts = [
+        'title' => 'string',
+        'content' => 'string',
+        'user_id' => 'integer'
+    ];
 }
