e4a65c52bd6d2e89aec52a4d8b6631f006f83556
Reem Ahmad <reemahmadn22@gmail.com>
2025-04-08 14:23:54 +0300

    Add Try-Cache for Login/Register

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -11,56 +11,73 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Requests\LoginRequest;
 use App\Http\Resources\UserResource;
+use Illuminate\Support\Facades\Log;
 
 class AuthController extends Controller
 {
 
     public function register(RegisterRequest $request)
     {
-        // Add User
-        $user = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
-        ]);
+        try {
+            // Add User
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+            ]);
 
-        // token
-        $token = $user->createToken('auth_token')->plainTextToken;
+            // token
+            $token = $user->createToken('auth_token')->plainTextToken;
 
-        // Return Response
-        return ApiResponseService::success(
-            [
-                'user' => UserResource::make($user),
-                'authorisation' => [
-                    'token' => $token,
-                    'type' => 'bearer',
-                ]
-            ],
-            'Register successful',
-            201
-        );
+            // Return Response
+            return ApiResponseService::success(
+                [
+                    'user' => UserResource::make($user),
+                    'authorisation' => [
+                        'token' => $token,
+                        'type' => 'bearer',
+                    ]
+                ],
+                'Register successful',
+                201
+            );
+        } catch (\Exception $e) {
+            Log::error('Login Error: ' . $e->getMessage());
+            return ApiResponseService::error(
+                'Reister failed. Please try again later.',
+                500
+            );
+        }
     }
 
     // Login Controller
     public function login(LoginRequest $request)
     {
-        $credentials = $request->only('email', 'password');
+        try {
+            $credentials = $request->only('email', 'password');
 
-        if (!Auth::attempt($credentials)) {
-            return ApiResponseService::error('Unauthorized', 401);
+            if (!Auth::attempt($credentials)) {
+                return ApiResponseService::error('Unauthorized', 401);
+            }
+
+            // token & login
+            $user = Auth::user();
+            $token = $user->createToken('auth_token')->plainTextToken;
+
+            // Return Response
+            return ApiResponseService::success([
+                'user' => UserResource::make($user),
+                'authorisation' => [
+                    'token' => $token,
+                    'type' => 'bearer'
+                ]
+            ], 'Login successful', 200);
+        } catch (\Exception $e) {
+            Log::error('Login Error: ' . $e->getMessage());
+            return ApiResponseService::error(
+                'Login failed. Please try again later.',
+                500
+            );
         }
-
-        // token & login
-        $user = Auth::user();
-        $token = $user->createToken('auth_token')->plainTextToken;
-
-        // Return Response
-        return ApiResponseService::success([
-            'user' => UserResource::make($user),
-            'authorisation' => [
-                'token' => $token,
-                'type' => 'bearer'
-            ]
-        ], 'Login successful',200);
     }
 }
